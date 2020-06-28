62973c1d572e201e890699cdefbd9a76f5c947b4
Paula Sena <senasilvaanapaula@gmail.com>
2020-06-28 03:56:36 -0300

    CSS grid

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -18,25 +18,39 @@
 
       populateUFs() 
 
+      // const stateInput  esconder input para não aparecer dados na url -->
 
       function getCities(event) {
         const citySelect = document.querySelector("[name=city]")
-        const stateInput = document.querySelector("input[name=state]")
+        const stateInput = document.querySelector("[name=state]")
 
         const ufValue = event.target.value
 
+        const indexOfSelectedState=event.target.selectedIndex
+        stateInput.value=event.target.options[indexOfSelectedState].text
+
+
+    //    const indexOfSelectedCity=event.target.selectedIndex
+      //  citySelect.value=event.target.options[indexOfSelectedCity].text
+
         const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`     
+
+//Limpando campo para o carregamento
+citySelect.innerHTML ="<option value>Selecione a Cidade</option>"
+citySelect.disabled=true
+
 
         fetch(url)
         .then( res => res.json())
         .then( cities => {
 
           for( const city of cities ){
-            
-          citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+
+          citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
           }
 
           citySelect.disabled=false
+ 
 
 
           })
@@ -51,6 +65,15 @@ document
     .addEventListener("change", getCities)  
 
 
+
+  const itensToCollect = document.querySelectorAll('.itens-grid li')
+   for (const item of itensToCollect){
+     item.addEventListener("click",handleSelectedItem)
+   }
+
+   function handleSelectedItem(event){
+     console.log(event.target)
+   }
 
 // Execute a function populateUFs(){
 // carregado o no console f12 o evento.target e exibido o campo Utilizado.
