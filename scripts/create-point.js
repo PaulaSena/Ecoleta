937c8616a1b07c0747ffbf841f392f881353e3eb
Paula Sena <senasilvaanapaula@gmail.com>
2020-06-28 01:08:37 -0300

    Corrigido =)

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -20,28 +20,30 @@
 
 
       function getCities(event) {
-        const citySelect = document.querySelector("select[name=city]")
+        const citySelect = document.querySelector("[name=city]")
         const stateInput = document.querySelector("input[name=state]")
 
+        const ufValue = event.target.value
+
+        const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`     
+
+        fetch(url)
+        .then( res => res.json())
+        .then( cities => {
+
+          for( const city of cities ){
+            
+          citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+          }
+
+          citySelect.disabled=false
 
 
-      console.log(event.target.value)
-
-  //  const url =`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
-
-/*
-       fetch(url)
-       .then( res => res.json())
-       .then( cities => {
-
-         for( const city of cities ){
-           citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
-         }
-*/
-
-       
+          })
 
       }
+
+
 
       
 document
