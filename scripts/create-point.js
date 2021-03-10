00c1b9944f62d576d049c43416fde2c08f69eeb5
Ana Paula Sena da Silva <45314696+PaulaSena@users.noreply.github.com>
2021-03-10 15:35:39 -0300

    Carregado corretamente o estado da API

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -15,13 +15,67 @@
 
       populateUFs() 
 
+         // const stateInput  esconder input para não aparecer dados na url -->
+
 function getCities(event){
 
+
+      function getCities(event) {
+        const citySelect = document.querySelector(select"[name=city]")
+        const stateInput = document.querySelector(select"[name=state]")
+        
+         const ufValue = event.target.value
+
+        const indexOfSelectedState = event.target.selectedIndex;
+        stateinput.value = event.target.options[indexOfSelectedState].text;
 }
 
 
       
+
+       //  const indexOfSelectedCity=event.target.selectedIndex
+       //  citySelect.value=event.target.options[indexOfSelectedCity].text
+
+        const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`     
+
+                //Limpando campo para o carregamento
+                citySelect.innerHTML ="<option value>Selecione a Cidade</option>"
+                citySelect.disabled=true
+
+
+        fetch(url)
+        .then( res => res.json())
+        .then( cities => {
+
+          for( const city of cities ){
+
+          citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
+          }
+
+          citySelect.disabled=false
+ 
+          })
+
+      }
+
+     
 document
     .querySelector("select[name=uf]")
-    .addEventListener("change",getCities)
-    })
+    .addEventListener("change", getCities)  
+
+    // itens de coleta all li
+
+  const itensToCollect = document.querySelectorAll('.itens-grid li')
+   for (const item of itensToCollect){
+     item.addEventListener("click",handleSelectedItem)
+    }
+
+    const collectedItens = document.querySelector("input[name=itens]");
+    let selectedItens = [];
+
+   function handleSelectedItem(event){
+    // console.log(event.target)
+       const itemLi = event.target;
+       itemLi.classList.toggle("selected");
+       const itemId = eventLi.dataset.id;
+   }
